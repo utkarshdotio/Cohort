0efdc1ae4bc9c260ffb6e4194e4aa716466a81ce
Utkarsh Saxena <saxenautkarsh2001@gmail.com>
2026-04-11 20:53:09 +0530

    fix: correct color code format in random color generation

--- Projects/JS/D43/script.js
+++ Projects/JS/D43/script.js
@@ -7,7 +7,7 @@ btn.addEventListener('click',function(){
     var c2=Math.floor(Math.random()*256)
     var c3=Math.floor(Math.random()*256)
     console.log(c1,c2,c3);
-    var colCode= `${c1},${c2},${c3}`    
+    var colCode= `rgb(${c1},${c2},${c3})`    
     h2.innerHTML=colCode
     
     box.style.backgroundColor=`rgb(${c1},${c2},${c3})`
