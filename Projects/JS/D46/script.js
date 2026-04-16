d7bcd9feaee68910081d0f01380eb58361a0ec38
Utkarsh Saxena <saxenautkarsh2001@gmail.com>
2026-04-17 01:47:33 +0530

    Refactor code structure for improved readability and maintainability

---
+++ Projects/JS/D46/script.js
@@ -0,0 +1,10 @@
+var img=document.querySelector('img')
+var love=document.querySelector('#love')
+
+img.addEventListener('click',function(){
+    love.style.opacity=1;
+    love.style.transform='translate(-50%,-50%) scale(1.3)'
+    
+    
+})
+
